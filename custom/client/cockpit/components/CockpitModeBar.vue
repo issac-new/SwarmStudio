2269b55a46913f3aa0d6097ad5b1da75bb3fc0aa
plusprimer <plusprimer@me.com>
2026-06-24 16:42:38 +0800

    feat: replace Chat button with Code button in mode bar

--- custom/client/cockpit/components/CockpitModeBar.vue
+++ custom/client/cockpit/components/CockpitModeBar.vue
@@ -49,9 +49,9 @@ function onRightMaximize() {
     <button
       type="button"
       class="cockpit-mode-bar__mode"
-      :class="{ 'is-on': store.workspaceMode === 'chat' }"
-      @click="store.setWorkspaceMode('chat')"
-    >💬 {{ t('cockpit.modeChat') }} <span v-if="store.channelsForSelectedTask.length" class="cockpit-mode-bar__count">{{ store.channelsForSelectedTask.length }}</span></button>
+      :class="{ 'is-on': store.workspaceMode === 'term' }"
+      @click="store.enterTerminal()"
+    >⌘ {{ t('cockpit.modeTerm') }}</button>
     <span class="cockpit-mode-bar__spacer" />
     <button
       type="button"
